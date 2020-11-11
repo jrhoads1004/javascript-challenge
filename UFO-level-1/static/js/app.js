46ef68c28170f68a82be106fbc6880a5db7e0789
Joshua Rhoads <josh@MacBook-Pro.local>
2020-11-10 23:06:57 -0600

    updated code to filter data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,68 +1,56 @@
 // from data.js
 var tableData = data;
-console.log(tableData);
+
 // YOUR CODE HERE!
 
-
-// select table
 var tbody = d3.select("tbody");
 
 
-//append all data to table
-tableData.forEach(function(UFOsighting) {
-	console.log(UFOsighting);
-	var row = tbody.append("tr");
-		Object.entries(UFOsighting).forEach(function([key,value]){
-			//console.log(`${key} : ${value}`);		
-			row.append("td").text(value);
-			});
-	});
+tableData.forEach((UFO_sighting) => {
+  var row = tbody.append("tr");
+  Object.entries(UFO_sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
 
-//filter by date search
-
-//declare button and input
 var button = d3.select("#filter-btn");
-var input = d3.select("#datetime");
+var reset_button = d3.select("#reset-btn");
 
-//function that will filter the data based on input
-function filterclick(){
-	//prevent page refresh
-	d3.event.preventDefault();
-	console.log("button was clicked");
-	//input value
-	var inputValue = input.property("value");
-	console.log(`input : ${inputValue}`);
-	console.log(d3.event.target);
-	//clear field
-	tbody.html("");
-	//filter from input
-	var filterDate = tableData.filter(tableData => tableData.datetime === inputValue);
-	
-	
-	
-	//create new table with values from only filter date
-	//append filtered row and values
-	//same as previous code
-	
-		//create msg if filter 	
-		if (filterDate.length == 0){
-			var thead = d3.select("thead");
-			var row = tbody.append("tr");
-			row.append("td").text("No sightings on this date");
-			
-			
-   
-			}
-		else {
-			filterDate.forEach(function(UFOsighting) {
-				console.log(UFOsighting);
-				var row = tbody.append("tr");
-					Object.entries(UFOsighting).forEach(function([key,value]){
-						//console.log(`${key} : ${value}`);		
-						row.append("td").text(value);
-						});
-		});}
-}
+button.on("click", function() {    
+    
+  var inputDate = d3.select("#datetime");
+  var inputValue = inputDate.property("value");
 
-//click button and run function
-button.on("click", filterclick);
+  console.log(inputValue);
+  console.log(tableData);
+  
+  var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+  console.log(filteredData);
+    
+  tbody.html("");
+  
+  filteredData.forEach((UFO_sighting) => {
+  var row = tbody.append("tr");
+  Object.entries(UFO_sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+  });
+  });
+
+reset_button.on("click", function() {    
+    
+    console.log("Resetting Table");
+    
+    tbody.html("");
+  
+    tableData.forEach((UFO_sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(UFO_sighting).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+    });
